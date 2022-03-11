7e97812e0a68558bba0fd6274678fb0f55dd3d74
Suyog Deshmukh <Suyog Deshmukh@DESKTOP-Q5OK82B>
2022-03-11 12:59:47 +0530

    added Exception2.java

---
+++ ExceptionHandling/src/Exception2.java
@@ -0,0 +1,9 @@
+
+public class Exception2 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
--- ExceptionHandling/src/Exception_demo1.java
+++ ExceptionHandling/src/Exception_demo1.java
@@ -3,7 +3,29 @@ public class Exception_demo1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		try 
+        {
+            System.out.print("A");
+            int num = 99/0;
+            System.out.print("B");
+            System.out.println("Print :" +num);
+        }
+        catch(ArithmeticException ex) 
+        {
+	       
+            System.out.print("C");
+        }
+        catch(Exception ex)
+        {
+            System.out.print("D");
+        }
+        System.out.print("E");
+
+
+
+}
+
 
 	}
 
-}
+
